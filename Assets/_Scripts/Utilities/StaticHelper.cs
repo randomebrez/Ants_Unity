8cc9d778a5f79bb7ca5b2bfc578d8703a8646097
Salocin <Salocin@DESKTOP-LMAUDEI>
2022-02-19 19:46:01 +0100

    Add static helper

---
+++ Assets/_Scripts/Utilities/StaticHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts.Utilities
+{
+    public static class StaticHelper
+    {
+        public static void SetRecursiveLayer(List<GameObject> objects, int layerId)
+        {
+            if (objects.Count == 0)
+                return;
+
+            var newList = new List<GameObject>();
+
+            foreach (var obj in objects)
+            {
+                obj.layer = layerId;
+
+                for (int i = 0; i < obj.transform.childCount; i++)
+                {
+                    newList.Add(obj.transform.GetChild(i).gameObject);
+                }
+            }
+
+            SetRecursiveLayer(newList, layerId);
+        }
+    }
+}
